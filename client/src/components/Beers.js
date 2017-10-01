44bb9d2bbb3f362e37a261038dc2044bbbc7d748
TJ Checketts <tylerchecketts@gmail.com>
2017-10-01 11:46:27 -0600

    update notes

--- client/src/components/Beers.js
+++ client/src/components/Beers.js
@@ -1,13 +1,47 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import axios from 'axios';
+import { Card, Image, Divider } from 'semantic-ui-react';
+
+let { id, name, images = {} } = brewery;
 
 class Beers extends React.Component {
-  
+  state = brewery_db.beers.all
   // write call beers function
-  
+  componentDidMount() {
+    axios.get('/api/all_beers')
+      .then (res => {this.setState({ beers: res.data })}
+    )
+      .catch( error => {
+        console.log(error.response);
+    });
+  }
+
+  show = () => {
+    <div>
+      {this.state.beers.map( b =>
+        <div> key={b.id}>
+          <Link to={`beers/${b.id}`}>
+            {b.name}
+            {' '}
+          </Link>
+        </div>
+      )}
+    </div>
+  }
+
   render() {
+    const { showForm } = this.state;
+    let { beers } = this.state;
+    
     return(
       <div>
-        Beers Component
+        {this.state.all_beers.map( b =>
+        <div> key={b.id}>
+            {b.name}
+            {' '}
+        </div>
+      )}
       </div>
     )
   }
--- notes.md
+++ notes.md
@@ -1,3 +1,15 @@
+start: 10/1 10 AM
+  set up show info on beers
+  learned that our db is online
+    reading about how this works
+    found these
+      https://github.com/tylerhunt/brewery_db
+      https://github.com/cmar/brewerydb
+  tried a couple different things on beers.js, but didn't work
+  found this article
+    https://blog.hellojs.org/fetching-api-data-with-react-js-460fe8bbf8f2
+stop: 11:45 PM
+
 start: 9/29 1:45 PM
   add api key and .env
   add beers component and route
@@ -6,4 +18,4 @@ start: 9/29 1:45 PM
   didn't run "yarn" inside of client
   bundle exec foreman now working
   fix beers component and route
-stop:  
+stop: 2:30 PM
